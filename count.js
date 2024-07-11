ed1c495dd95d3215845bbb5b81f4a71f0e67923b
stasbychkar <124491201+stasbychkar@users.noreply.github.com>
2024-07-11 12:54:41 -0700

    pre-final test

--- count.js
+++ count.js
@@ -1,29 +1,31 @@
 document.addEventListener('DOMContentLoaded', () => {
 
-    // count total cash
-    document.querySelector('form').onsubmit = () => {
-        let cash_sum = 0;
-        document.querySelectorAll('.count').forEach((input) => {
-            cash_sum += count(input.value) * parseFloat(input.dataset.coin);
-        });
-        let total = document.querySelector('#total');
-        let result = document.querySelector('#result');
-        totalNum = cash_sum;
-        let resultNum = totalNum;
-        // which drawer we are using ...
-        if (document.querySelector("#checkbox_toggle").checked) {
-            resultNum -= 300;
-        } else {
-            resultNum -= 200;
+    if (document.querySelector('form')) {
+        // count total cash
+        document.querySelector('form').onsubmit = () => {
+            let cash_sum = 0;
+            document.querySelectorAll('.count').forEach((input) => {
+                cash_sum += count(input.value) * parseFloat(input.dataset.coin);
+            });
+            let total = document.querySelector('#total');
+            let result = document.querySelector('#result');
+            totalNum = cash_sum;
+            let resultNum = totalNum;
+            // which drawer we are using
+            if (document.querySelector("#checkbox_toggle").checked) {
+                resultNum -= 300;
+            } else {
+                resultNum -= 200;
+            }
+
+            result.innerHTML = resultNum.toFixed(2);
+            total.innerHTML = totalNum.toFixed(2);
+
+            return false;
+        
         }
-
-        result.innerHTML = resultNum.toFixed(2);
-        total.innerHTML = totalNum.toFixed(2);
-
-        return false;
-    
     }
-
+        
     // sync count -> amount
     document.querySelectorAll('[data-count]').forEach((inputField) => {
         inputField.addEventListener('change', () => {
@@ -43,8 +45,22 @@ document.addEventListener('DOMContentLoaded', () => {
         });
     });
 
-    // ? bills selection 
-    // choose drawer
+    // --CI Count--
+    
+    document.querySelectorAll('.input-of-two').forEach((inputField) => {
+        let total_lb = 0;
+        // getting item's name
+        inputField.addEventListener('keyup', () => {
+            let inputClass = inputField.className.split(' ')[1];
+            total_lb = 0;
+            document.querySelectorAll(`.${inputClass}`).forEach(input => {
+                total_lb += count(input.value) * parseFloat(input.dataset.lb);
+            });
+            console.log(total_lb);
+            let total_html = document.querySelector(`.${inputClass}-total`);
+            total_html.innerHTML = total_lb.toFixed(1);
+        });
+    });
 })
 
 // helper functions
